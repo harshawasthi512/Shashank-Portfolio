083a2872b818a639c36991e656ec0f3a018f8c4d
harshawasthi512 <166531604+harshawasthi512@users.noreply.github.com>
2024-08-12 14:54:12 +0530

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,68 @@
+
+var linkedin = "https://www.linkedin.com/in/shashank-shekhar-awasthi-3697a11b7/";
+var email = "mailto:shashanka265@gmail.com";
+
+aboutme = [
+  "<br>",
+  "<span class='underline'>Hey, I'm Shashank!👋</span>",
+  "<br>",
+  "<li>👨🏻‍💻 As a dedicated Associate Information Security professional at eSec Forte Technologies,<br>I am passionate about ensuring online safety by identifying and resolving vulnerabilities. <br>My journey in the realm of cybersecurity began with an internship at Cyber Zone Technologies,<br> where I honed my skills in web application penetration testing and networking.</li><br>",
+  
+  "<li>🐞Since 2020, I've been actively engaged in bug bounty hunting on platforms like Bugcrowd and HackerOne,<br> which has significantly enriched my approach to bug hunting and bolstered my corporate career.<br> My academic background includes a Master of Computer Applications from Baba Saheb Bhimrao Ambedkar University<br> and a Bachelor of Computer Applications from Lucknow Public College of Professional Studies.</li>",
+  `<pre class="whitespace-pre-wrap">
+        `,
+  `<li><pre class="whitespace-pre-wrap">🎯  Throughout my career, I've had the privilege of securing websites for renowned companies, including<br>    - Meta,<br>    - Zomato,<br>    - Amazon,<br>    - Mozilla,<br>    - Samsung,<br>    - Mastercard,<br> among others.<br><br> 🏆My contributions have been recognized with the <b>Rising Star</b> award from my current company,<br> where I have submitted over <b>100</b> critical and high vulnerabilities.<br>`,
+  `<li><pre class="whitespace-pre-wrap"><br>👨‍💻 With expertise in web application pentesting, API testing, and mobile testing,<br> I am committed to making the internet a safer place.<br> My interests lie in exploring the internet, hunting on daily-use applications,<br> and continually expanding my knowledge through reading and creating write-ups.`,
+  
+  "<br>",
+];
+
+
+social = [
+  "<br>",
+  'linkedin       <a href="https://www.linkedin.com/in/shashank-shekhar-awasthi-3697a11b7/' +
+    linkedin +
+    '" target="_blank">linkedin/shashank-shekhar-awasthi' +
+    "</a>",
+  'email          <a href="mailto:shashanka265@gmail.com" target="_blank">shashanka265@gmail.com' +
+    "</a>",
+  'whatsapp       <a href="https://wa.me/6394266190" target="_blank">+91 6394266190' +
+    "</a>",
+  "<br>",
+];
+
+
+projects = [
+  "<br>",
+  `<a href="https://github.com/cosmicwanderer7/google-clone" target="blank"><span class='underline'>Care Health Insaurance </span>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap">The Care Health Insurance project streamlines health insurance selection by allowing users to compare and choose plans easily. Built with a user-friendly interface, it offers personalized recommendations and secure transactions. The platform ensures a seamless browsing experience with a responsive design, helping users make informed insurance decisions efficiently.
+
+`,
+  "<br>",
+,
+
+];
+
+help = [
+  `<br><pre class="whitespace-pre-wrap">
+  <span class="command">aboutme</span>    
+   <span class="command">↳</span>&nbsp;Diplays Who I am?
+  <span class="command">social</span>     
+   <span class="command">↳</span>&nbsp;Lists social networks.
+  <span class="command">projects</span>   
+   <span class="command">↳</span>&nbsp;View coding projects.
+  <span class="command">email</span>      
+   <span class="command">↳</span>&nbsp;To send me a email.
+  <span class="command">history</span>    
+   <span class="command">↳</span>&nbsp;View command history.
+  <span class="command">help</span>       
+   <span class="command">↳</span>&nbsp;Displays this help msg.
+  <span class="command">sudo</span>       
+   <span class="command">↳</span>&nbsp;Try it out for yourself.
+  <span class="command">clear</span>
+   <span class="command">↳</span>&nbsp;Clear terminal.</pre><br>`,
+];
+banner = [
+  `<div class="pt-2"><span class="text-[#7d82d7db] ">Welcome to my portfolio! — Type <span class="command text-[#75e1e7]">help</span> for a list of supported commands.</span></div>`,
+];
